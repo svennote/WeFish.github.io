3f66cd1cf5f00d59a5fde3abc9a0075fba015a45
Sven Noteboom <Sven Noteboom>
2016-12-20 11:15:24 +0100

    aangepast

--- js/map.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-  var mapCanvas = document.getElementsByClassName("stedenwrapper");
-  var mapOptions = {
-    center: new google.maps.LatLng(51,3.8),
-    zoom: 10
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
