6d8d92b3cba424c2789e27a96377964cff767594
Sven Noteboom <Sven Noteboom>
2016-12-20 11:13:27 +0100

    aangepast

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 function myMap() {
-  var mapCanvas = document.getElementById("map");
+  var mapCanvas = document.getElementsByClassName("stedenwrapper");
   var mapOptions = {
     center: new google.maps.LatLng(51,3.8),
     zoom: 10
