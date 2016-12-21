3fe0abe42b3410dc4b47ade5a2b720dac1df2c76
Sven Noteboom <Sven Noteboom>
2016-12-21 13:56:21 +0100

    aangepast

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     woeid: '',
     unit: 'C',
     success: function (weather) {
-      html += '<h4>' + weather.code + '' + weather.temp + '&deg;' + weather.units.temp + '</h4>';
+      html += '<h4>' + weather.code + weather.temp + '&deg;' + weather.units.temp + '</h4>';
       html += '<ul><li><h4>' + weather.city + ', ' + weather.region + '</h4></li>';
       html += '<li class="currently">' + weather.currently + '</li>';
       html += '<li><i class="fa fa-compass" aria-hidden="true"></i> ' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
