3aede18abb8e1cade41b9fb981a53e6574288e7e
Sven Noteboom <Sven Noteboom>
2016-12-21 18:25:29 +0100

    aangepast

--- js/brugge.js
+++ js/brugge.js
@@ -8,8 +8,7 @@ $(document).ready(function () {
     success: function (weather) {
       html += '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
       html += '<ul><li><h4>' + weather.city + ', ' + weather.region + '</h4></li>';
-      html += '<li class="currently">' + weather.currently + '</li>';
-      html += '<li><i class="fa fa-compass" aria-hidden="true"></i> ' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
+      html += '<li class="currently">' + weather.currently + '<li><i class="fa fa-compass" aria-hidden="true"></i> ' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
 
       $("#weather").html(html);
     },
