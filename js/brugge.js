e196babe732f30829de2e5db108fb5e51887fae9
Sven Noteboom <Sven Noteboom>
2016-12-21 17:45:38 +0100

    aangepast

---
+++ js/brugge.js
@@ -0,0 +1,20 @@
+var html = '';
+var tempStr = '';
+$(document).ready(function () {
+  $.simpleWeather({
+    location: 'Bruges, West-Vlaanderen',
+    woeid: '',
+    unit: 'C',
+    success: function (weather) {
+      html += '<h4><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h4>';
+      html += '<ul><li><h4>' + weather.city + ', ' + weather.region + '</h4></li>';
+      html += '<li class="currently">' + weather.currently + '</li>';
+      html += '<li><i class="fa fa-compass" aria-hidden="true"></i> ' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
+
+      $("#weather").html(html);
+    },
+    error: function (error) {
+      $("#weather").html('<p>' + error + '</p>');
+    }
+  })
+});
